34da38d2229d20d7f9336c455b8110eb605e04b8
FETC_iMac2 <TommySTsai@fetc.net.tw>
2022-05-05 10:26:58 +0800

    登入功能

--- SourceTreeDemo2_2/SourceTreeDemo2_2/ViewController.swift
+++ SourceTreeDemo2_2/SourceTreeDemo2_2/ViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var logIn = "登入"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup  after loading the view.
     }
 
 
